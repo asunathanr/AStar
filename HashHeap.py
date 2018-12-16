c5897e36acc8ba38d0b9e8268679cad20a7a1da1
Nathan Robertson <absnathanr@gmail.com>
2018-12-16 08:52:51 -0600

    Added basic pop

--- HashHeap.py
+++ HashHeap.py
@@ -4,6 +4,7 @@ import heapq
 class HashHeap:
     """
     A structure which allows both fast search and fast heaping.
+    The heap structure controls addition/removal of items
     Found this idea at: https://github.com/jonasnick/A-star/blob/master/astar/datastructures/HashPriorityQueue.java
     """
     def __init__(self):
@@ -14,11 +15,12 @@ class HashHeap:
         heapq.heappush(self.heap, (key, value))
         self.table[key] = value
 
-    def remove(self, key):
-        pass
-
     def pop(self):
-        return heapq.heappop(self.heap)
+        key, value = heapq.heappop(self.heap)
+        if self.find(key) is not None:
+            value = self.table[key]
+            self.table.pop(key)
+        return key, value
 
     def find(self, key):
         if key in self.table:
--- hash_heap_test.py
+++ hash_heap_test.py
@@ -9,3 +9,10 @@ class HashHeapTest(unittest.TestCase):
     def test_add_item(self):
         self.hash_heap.add(1, 2)
         self.assertEqual(2, self.hash_heap.find(1))
+
+    def test_pop(self):
+        self.hash_heap.add(1, 2)
+        key, value = self.hash_heap.pop()
+        self.assertEqual(1, key)
+        self.assertEqual(2, value)
+        self.assertIsNone(self.hash_heap.find(1))
