6160e4d53461cf25a2531127986368f28e6a7852
Nathan Robertson <absnathanr@gmail.com>
2018-12-16 08:57:40 -0600

    Verified pop returns correct item

--- hash_heap_test.py
+++ hash_heap_test.py
@@ -7,6 +7,10 @@ class HashHeapTest(unittest.TestCase):
         self.hash_heap = HashHeap()
 
     def test_add_item(self):
+        """
+        Add item to heap and find it later
+        :return:
+        """
         self.hash_heap.add(1, 2)
         self.assertEqual(2, self.hash_heap.find(1))
 
@@ -16,3 +20,13 @@ class HashHeapTest(unittest.TestCase):
         self.assertEqual(1, key)
         self.assertEqual(2, value)
         self.assertIsNone(self.hash_heap.find(1))
+
+    def test_min_prop(self):
+        """
+        Test min heap property that smallest item is first to come off.
+        :return:
+        """
+        self.hash_heap.add(1, 2)
+        self.hash_heap.add(3, 2)
+        key, value = self.hash_heap.pop()
+        self.assertEqual(1, key)
