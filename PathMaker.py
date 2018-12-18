1bca54d39b7347f6f40cdda1cb4520d6874d8083
Nathan Robertson <absnathanr@gmail.com>
2018-12-18 09:53:08 -0600

    Removed AStar nesting

--- PathMaker.py
+++ PathMaker.py
@@ -1,6 +1,7 @@
 from weighted_coord import Coord, WeightedCoord
 from HashHeap import HashHeap
 
+
 # File: AStar.py
 # Authors: Eric Day, Hey Joe, Nathan Robertson
 # Purpose: Implement the A* algorithm using the MapGrid class
@@ -22,7 +23,7 @@ class PathMaker:
             return self.SAME_CELL
         if not self.is_valid_coord(start) or not self.is_valid_coord(end):
             return self.PATH_OUT_OF_BOUNDS
-        last_cell, successful = self.AStar(self.grid, (start, end), self.heuristic_fn).execute()
+        last_cell, successful = AStar(self.grid, (start, end), self.heuristic_fn).execute()
         path = self.make_path(last_cell, end) if successful else self.INVALID_PATH
         return self.remove_weights(path)
 
@@ -51,82 +52,83 @@ class PathMaker:
             return []
         return self.find_path(weighted_end.parent) + [weighted_end]
 
-    class AStar:
+
+class AStar:
+    """
+    Creates paths from start to one goal using a heuristic function and a grid structure.
+    This is the meat of the A* implementation.
+    """
+
+    def __init__(self, grid, endpoints: (Coord, Coord), heuristic_fn):
+        self.grid = grid
+        self.heuristic_fn = heuristic_fn
+        self.start, self.end = endpoints
+        self.open_set = HashHeap()
+        self.open_set.add(0, WeightedCoord(0, self.start.x, self.start.y))
+        self.closed_set = {}
+        self.setup_closed_set()
+        self.current = self.open_set.top()
+        self.neighbors = self.next_neighbors()
+
+    def execute(self) -> (WeightedCoord, bool):
         """
-        Creates paths from start to one goal using a heuristic function and a grid structure.
-        This is the meat of the A* implementation.
+        Attempt to find quickest path from start to end.
+        This starts the "procedural" part of the algorithm. It seems best to treat this part procedurally so far.
+        :return: Next to last node of path and if an actual path was found from start to end
         """
-        def __init__(self, grid, endpoints: (Coord, Coord), heuristic_fn):
-            self.grid = grid
-            self.heuristic_fn = heuristic_fn
-            self.start, self.end = endpoints
-            self.open_set = HashHeap()
-            self.open_set.add(0, WeightedCoord(0, self.start.x, self.start.y))
-            self.closed_set = {}
-            self.setup_closed_set()
+        while len(self.open_set) > 0 and self.end not in self.neighbors:
+            self.open_set.pop()
+            self.process_cell()
             self.current = self.open_set.top()
             self.neighbors = self.next_neighbors()
+        return self.current, self.end in self.neighbors
 
-        def execute(self) -> (WeightedCoord, bool):
-            """
-            Attempt to find quickest path from start to end.
-            This starts the "procedural" part of the algorithm. It seems best to treat this part procedurally so far.
-            :return: Next to last node of path and if an actual path was found from start to end
-            """
-            while len(self.open_set) > 0 and self.end not in self.neighbors:
-                self.open_set.pop()
-                self.process_cell()
-                self.current = self.open_set.top()
-                self.neighbors = self.next_neighbors()
-            return self.current, self.end in self.neighbors
+    def process_cell(self):
+        """
+        Given the current cell and neighbors not in closed set for that cell update open set with new information.
+        Add it to closed set once finished
+        """
+        cheaper_neighbors = self.neighbors_to_update()
+        new_cells = map(lambda neighbor: self.make_new_cell(neighbor), cheaper_neighbors)
+        self.add_new_cells(new_cells)
+        self.closed_set[Coord(self.current.x, self.current.y)] = True
 
-        def process_cell(self):
-            """
-            Given the current cell and neighbors not in closed set for that cell update open set with new information.
-            Add it to closed set once finished
-            """
-            cheaper_neighbors = self.neighbors_to_update()
-            new_cells = map(lambda neighbor: self.make_new_cell(neighbor), cheaper_neighbors)
-            self.add_new_cells(new_cells)
-            self.closed_set[Coord(self.current.x, self.current.y)] = True
+    def add_new_cells(self, new_cells) -> None:
+        """
+        Add cells that are potentially part of the cheapest path.
+        :param new_cells:
+        """
+        for cell in new_cells:
+            self.open_set.add(cell.weight, cell)
 
-        def add_new_cells(self, new_cells) -> None:
-            """
-            Add cells that are potentially part of the cheapest path.
-            :param new_cells:
-            """
-            for cell in new_cells:
-                self.open_set.add(cell.weight, cell)
+    def next_neighbors(self) -> []:
+        neighbors = list(filter(lambda n: not self.closed_set[n], self.grid.neighbors(self.current)))
+        return neighbors
 
-        def next_neighbors(self) -> []:
-            neighbors = list(filter(lambda n: not self.closed_set[n], self.grid.neighbors(self.current)))
-            return neighbors
+    def neighbors_to_update(self):
+        calc_g = lambda n: self.current.weight + self.grid.cost(n)
+        return filter(lambda x: self.should_replace_cell(calc_g(x)), self.neighbors)
 
-        def neighbors_to_update(self):
-            calc_g = lambda n: self.current.weight + self.grid.cost(n)
-            return filter(lambda x: self.should_replace_cell(calc_g(x)), self.neighbors)
+    def should_replace_cell(self, new_g):
+        old_value = self.open_set.find(self.current)
+        if old_value is None:
+            return True
+        if self.current == old_value and new_g < old_value.weight:
+            return True
+        return False
 
-        def should_replace_cell(self, new_g):
-            old_value = self.open_set.find(self.current)
-            if old_value is None:
-                return True
-            if self.current == old_value and new_g < old_value.weight:
-                return True
-            return False
+    def make_new_cell(self, location) -> WeightedCoord:
+        new_cell = WeightedCoord(self.current.weight + self.grid.cost(location), location.x, location.y, self.current)
+        dx1 = location.x - self.end.x
+        dy1 = location.y - self.end.y
+        dx2 = self.start.x - self.end.x
+        dy2 = self.start.y - self.start.x
+        cross = abs(dx1 * dy2 - dx2 * dy1)
+        new_cell.h = self.heuristic_fn(location, self.end) + cross * 0.001
+        new_cell.set_f()
+        return new_cell
 
-        def make_new_cell(self, location) -> WeightedCoord:
-            new_cell = WeightedCoord(self.current.weight + self.grid.cost(location), location.x, location.y, self.current)
-            dx1 = location.x - self.end.x
-            dy1 = location.y - self.end.y
-            dx2 = self.start.x - self.end.x
-            dy2 = self.start.y - self.start.x
-            cross = abs(dx1 * dy2 - dx2 * dy1)
-            new_cell.h = self.heuristic_fn(location, self.end) + cross * 0.001
-            new_cell.set_f()
-            return new_cell
-
-        def setup_closed_set(self):
-            for i in range(0, self.grid.xsize):
-                for j in range(0, self.grid.ysize):
-                    self.closed_set[Coord(i, j)] = False
-
+    def setup_closed_set(self):
+        for i in range(0, self.grid.xsize):
+            for j in range(0, self.grid.ysize):
+                self.closed_set[Coord(i, j)] = False
